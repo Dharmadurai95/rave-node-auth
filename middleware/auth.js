801e17bd4d8c52b4860dbe733de9d12fdeb58b8c
Dharmadurai Muthusamy <dharmadurai@autonom8.com>
2021-08-19 20:30:21 +0530

    add isValid token condition

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ const verifyToken =async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
      let isValidToken =await  User.findOne({token});
-     if(isValidToken.token !== token)res.status(401).send("Invalid Token");
+     if(isValidToken.token !== token)return res.status(401).send("Invalid Token");
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
